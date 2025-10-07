eabcd3006d53e07ef97a71434a42e23790405ea7
arfaar <arfaar283@gmail.com>
2025-10-07 23:14:34 +0400

    firstcommit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function showSubscriberThanks(){
+        alert("Thank you for subscribing to your Newsletter :)")
+}
+
+function showMessageReceived(){
+    alert("Thank you for your Message. We have received and we will get back to you soon")
+}
